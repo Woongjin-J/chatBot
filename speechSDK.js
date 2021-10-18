60c283273a8b76c82251d246d16e5d6d3cd4c9cd
Woongjin-J <67777378+Woongjin-J@users.noreply.github.com>
2021-10-19 08:56:37 +0900

    weather api
    weather api connected and can return the weather value

--- speechSDK.js
+++ speechSDK.js
@@ -1,15 +1,14 @@
 (function() {
   "use strict";
 
-  // import * as sdk from "microsoft-cognitiveservices-speech-sdk";
-
-  // subscription key and region for speech services.
-  var speechKey = "f470e25b6841498883626459deb9a1ba";
-  var subscriptionKey = "e7531fd877724d46b2395cb8cf7adfe6";
-  var serviceRegion = "westus";
-  var appId = "90172fe8-d914-4019-ae6a-e148ac29d755";
-  var SpeechSDK;
-  var recognizer;
+  let SpeechSDK;
+  let recognizer;
+  const speechKey = "f470e25b6841498883626459deb9a1ba";
+  const subscriptionKey = "e7531fd877724d46b2395cb8cf7adfe6";
+  const serviceRegion = "westus";
+  const appId = "90172fe8-d914-4019-ae6a-e148ac29d755";
+  const ONECALL_URL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid=acf380c77f1250015c7e020d4957ee34";
+  const COORD_URL = "https://api.openweathermap.org/data/2.5/weather?q=incheon&units=metric&APPID=acf380c77f1250015c7e020d4957ee34";
 
   document.addEventListener("DOMContentLoaded", init);
 
@@ -44,16 +43,16 @@
     // Set up a Language Understanding Model from Language Understanding Intelligent Service (LUIS).
     // See https://www.luis.ai/home for more information on LUIS.
     if (appId !== "" && appId !== "YOUR_LANGUAGE_UNDERSTANDING_APP_ID") {
-      var lm = SpeechSDK.LanguageUnderstandingModel.fromAppId(appId);
+      let lm = SpeechSDK.LanguageUnderstandingModel.fromAppId(appId);
 
       recognizer.addAllIntents(lm);
     }
 
-    recognizer.recognizeOnceAsync(recognize, errCheck);
+    recognizer.recognizeOnceAsync(recognize, error);
   }
 
   function recognize(result) {
-    var jsonResult;
+    let jsonResult;
     window.console.log(result);
     id("phraseDiv").innerHTML = result.text + "\r\n";
     id("statusDiv").innerHTML += "(continuation) Reason: " + SpeechSDK.ResultReason[result.reason] + "\r\n";
@@ -70,17 +69,16 @@
         // the entity name and type if part of the intent.
         jsonResult = result.properties.getProperty(SpeechSDK.PropertyId.LanguageUnderstandingServiceResponse_JsonResult);
         id("statusDiv").innerHTML += " Intent JSON: " + jsonResult + "\r\n";
-        id("phraseDiv").innerHTML += jsonResult + "\r\n";
         giveResponse(jsonResult);
         break;
 
       case SpeechSDK.ResultReason.NoMatch:
-        var noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(result);
+        let noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(result);
         id("statusDiv").innerHTML += " NoMatchReason: " + SpeechSDK.NoMatchReason[noMatchDetail.reason];
         break;
 
       case SpeechSDK.ResultReason.Canceled:
-        var cancelDetails = SpeechSDK.CancellationDetails.fromResult(result);
+        let cancelDetails = SpeechSDK.CancellationDetails.fromResult(result);
         id("statusDiv").innerHTML += " CancellationReason: " + SpeechSDK.CancellationReason[cancelDetails.reason];
 
         if (cancelDetails.reason === SpeechSDK.CancellationReason.Error) {
@@ -95,40 +93,93 @@
 
   function giveResponse(result) {
     result = JSON.parse(result);
-    var text = result.entities[0].entity + "\'s weather is 25 degree.\n" + result.topScoringIntent.intent;
-    id("respondDiv").innerHTML += text;
 
     const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(speechKey, serviceRegion);
     const synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
+    let intent = result.topScoringIntent.intent;
+    let entities = result.entities;
+    let text = "Sorry, I don't understand.";
 
-    synthesizer.speakTextAsync(text,
-      function(result) {
-        // const {audioData } = result;
-        synthesizer.close();
-        // const bufferStream = new PassThrough();
-        // bufferStream.end(Buffer.from(audioData));
-        // return bufferStream;
-        return result.audioData;
-      },
-      function(error) {
-        console.log(error);
-        synthesizer.close();
-      });
+    // let url = update_url(entities);
+    let url;
+
+    if (entities.length === 0 || (entities.length === 1 && (entities[0].type === "builtin.datetimeV2.datetime" ||
+                                                            entities[0].type === "builtin.datetimeV2.date"))) { // current location
+      navigator.geolocation.getCurrentPosition(function(pos) {
+          url = ONECALL_URL.replace('{lat}', pos.coords.latitude);
+          url = url.replace('{lon}', pos.coords.longitude);
+          fetch(url)
+            .then(checkStatus)
+            .then(JSON.parse)
+            .then(weather)
+            .catch();
+      }, error);
+    }
+    else { // specified location
+      let location = entities[0].entity;
+      url = COORD_URL.replace('{city}', location);
+      fetch(url)
+        .then(checkStatus)
+        .then(JSON.parse)
+        .then(info => {
+          url = ONECALL_URL.replace('{lat}', info.coord.lat);
+          url = url.replace('{lon}', info.coord.lon);
+          return fetch(url);
+        })
+        .then(checkStatus)
+        .then(JSON.parse)
+        .then(weather)
+        .catch();
+    }
+
+    function weather(info) {
+      console.log(info);
+      if (intent === "Weather.CheckWeatherValue") {
+        let condition = info.current.weather[0].main;
+        let len = entities.length;
+        let time;
+        let givenDate = new Date();
+        let today = new Date();
+        if (len > 0 && entities[len-1].type === "builtin.datetimeV2.date") {
+          time = entities[len-1].resolution.values[0].timex;
+          givenDate = new Date(time);
+        }
+
+        if (condition === "Clouds") {
+          condition = "cloudy";
+        } else if (condition === "Rain") {
+          condition = "raining";
+        } else if (condition === "Mist") {
+          condition = "foggy";
+        } else {
+          condition = condition.toLowerCase();
+        }
+
+        if (entities.length === 0 || time === "PRESENT_REF" || givenDate.getDate() === today.getDate()) { // current
+          text = "It's currently " + condition + " and the temperature is " + info.current.temp + " celcius degree.\n";
+        }
+        else if (givenDate < today) { // past
+          text = "It was " + condition + " and the temperature was " + info.current.temp + " celcius degree.\n";
+        }
+        else { // future
+          text = "It will be " + condition + " and the temperature will be " + info.current.temp + " celcius degree.\n";
+        }
+      }
+      id("respondDiv").innerHTML += text;
+
+      synthesizer.speakTextAsync(text,
+        function(result) {
+          synthesizer.close();
+          return result.audioData;
+        },
+        function(error) {
+          console.log(error);
+          synthesizer.close();
+        });
+    }
   }
 
-  // function speakResult(result) {
-  //   const { audioData } = result;
-
-  //   synthesizer.close();
-
-  //   // convert arrayBuffer to stream
-  //   // return stream
-  //   const bufferStream = new PassThrough();
-  //   bufferStream.end(Buffer.from(audioData));
-  //   return bufferStream;
-  // }
-
-  function errCheck(err) {
+  function error(err) {
     window.console.log(err);
     id("phraseDiv").innerHTML += "ERROR: " + err;
     id("talkButton").disabled = false;
@@ -146,5 +197,22 @@
    */
   function id(idName) {
     return document.getElementById(idName);
+  }
+
+  /**
+  *  Function to check the status of an Ajax call, boiler plate code to include,
+  *  based on: https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+  *  updated from
+  *  https://stackoverflow.com/questions/29467426/fetch-reject-promise-with-json-error-object
+  *  @param {Object} response the response text from the url call
+  *  @return {Object} did we succeed or not, so we know whether or not to continue with the
+  *  handling of this promise
+  */
+   function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response.text();
+    } else {
+      return Promise.reject(new Error(response.status + ": " + response.statusText));
+    }
   }
 })();
